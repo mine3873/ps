b26a3293180eccbec00061ed2106667595629548
Replit user <>
2023-06-04 11:09:37 +0000

    solved_3_problem

---
+++ .ccls-cache/@home@runner@ps/B5@2588.곱셈@2588.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+
+int main(){
+  int a, b;
+  std::cin >> a >> b;
+  std::cout << a * (b%10)  << "\n";
+  std::cout << a * (b/10 % 10) << "\n";
+  std::cout << a * (b/100) << "\n";
+  std::cout << a * b << "\n";
+  return 0;
+}
